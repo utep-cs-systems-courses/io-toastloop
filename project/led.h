f7d9113ac4600a9adafdfff2dd5b68dfc6cf66b8
toastloop <69254179+toastloop@users.noreply.github.com>
2023-10-15 12:16:52 -0600

    Updated led header file

--- project/led.h
+++ project/led.h
@@ -2,8 +2,10 @@
 #define  led_included
 
 void led_init(char LED);
+void led_close(char LED);
 void led_on(char LED);
 void led_off(char LED);
 void led_toggle(char LED);
+char led_status(char LED);
 
 #endif
