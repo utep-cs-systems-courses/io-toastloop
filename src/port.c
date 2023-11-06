0fa3a18e4864b84676fddd23f78f9bb652a796cf
toastloop <69254179+toastloop@users.noreply.github.com>
2023-11-06 04:45:37 -0700

    Added struct and functions for ports

---
+++ src/port.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <msp430.h>
+#include "port.h"
+
+port_t *portInit(unsigned char port){
+    port_t *p = malloc(sizeof(port_t));
+    switch(port){
+        case 1:
+            p->port = &P1OUT;
+            p->in = &P1IN;
+            p->out = &P1OUT;
+            p->dir = &P1DIR;
+            p->ren = &P1REN;
+            p->sel = &P1SEL;
+            p->sel2 = &P1SEL2;
+            p->ies = &P1IES;
+            p->ie = &P1IE;
+            p->ifg = &P1IFG;
+            break;
+        case 2:
+            p->port = &P2OUT;
+            p->in = &P2IN;
+            p->out = &P2OUT;
+            p->dir = &P2DIR;
+            p->ren = &P2REN;
+            p->sel = &P2SEL;
+            p->sel2 = &P2SEL2;
+            p->ies = &P2IES;
+            p->ie = &P2IE;
+            p->ifg = &P2IFG;
+            break;
+        default:
+            free(p);
+            return NULL;
+            break;
+    }
+    return p;
+}
+
+void port_close(port_t *p){
+    free(p);
+}
---
+++ src/port.h
@@ -0,0 +1,19 @@
+#IFNDEF __PORT_H__
+#DEFINE __PORT_H__
+
+#include <msp430.h>
+
+typedef struct {
+    unsigned char *port;
+    unsigned short *in;
+    unsigned short *out;
+    unsigned short *dir;
+    unsigned short *ren;
+    unsigned short *sel;
+    unsigned short *sel2;
+    unsigned short *ies;
+    unsigned short *ie;
+    unsigned short *ifg;
+} port_t;
+
+#ENDIF // __PORT_H__
